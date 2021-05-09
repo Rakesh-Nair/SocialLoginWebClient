229d5123d61ba2e7608ae361baed78879ee61226
Rakesh Nair <crucifyrakesh@gmail.com>
2021-05-09 12:01:26 +0530

    Updated websecurity and added logout

--- src/main/java/com/appsdeveloperblog/clients/sociallogin/SocialLoginWebClient/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/clients/sociallogin/SocialLoginWebClient/security/WebSecurity.java
@@ -1,13 +1,17 @@
 package com.appsdeveloperblog.clients.sociallogin.SocialLoginWebClient.security;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated().and().oauth2Login();
+		http.authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated().and().oauth2Login().and()
+				.logout().logoutSuccessUrl("/").invalidateHttpSession(true).clearAuthentication(true)
+				.deleteCookies("JSESSIONID");
 	}
 
 }
